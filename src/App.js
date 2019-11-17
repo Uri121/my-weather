79e4a53135141484b27a67fc9d2208193a7002bc
uri121 <robinov121@gmail.com>
2019-11-17 23:48:35 +0200

    update forecast

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React, { Component } from "react";
-import Weather from "./weather";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Weather from "./weather";
 import "weather-icons/css/weather-icons.css";
 import Form from "./form";
 import "./App.css";
 
-const API_Key =  process.env.REACT_APP_WEATHER_API_KEY;
+const API_Key = process.env.REACT_APP_WEATHER_API_KEY;
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       city: undefined,
-      icon: undefined,
-      main: undefined,
-      celsius: undefined,
-      temp_max: undefined,
-      temp_min: undefined,
-      description: "",
       error: false,
-      check:false
+      check: false,
+      weatherIcon: undefined,
+      fullData: [],
+      dailyData: []
     };
 
     this.weatherIcon = {
@@ -33,39 +30,6 @@ class App extends Component {
     };
   }
 
-  calCelsiuis(temp) {
-    let cell = Math.floor(temp - 275.15);
-    return cell;
-  }
-
-  getWeatherIcon(icons, rangeID) {
-    switch (true) {
-      case rangeID >= 200 && rangeID <= 232:
-        this.setState({ icon: this.weatherIcon.Thunderstorm });
-        break;
-      case rangeID >= 300 && rangeID <= 321:
-        this.setState({ icon: this.weatherIcon.Drizzle });
-        break;
-      case rangeID >= 500 && rangeID <= 531:
-        this.setState({ icon: this.weatherIcon.Rain });
-        break;
-      case rangeID >= 600 && rangeID <= 622:
-        this.setState({ icon: this.weatherIcon.Snow });
-        break;
-      case rangeID >= 701 && rangeID <= 781:
-        this.setState({ icon: this.weatherIcon.Atmosphere });
-        break;
-      case rangeID === 800:
-        this.setState({ icon: this.weatherIcon.Clear });
-        break;
-      case rangeID >= 801 && rangeID <= 804:
-        this.setState({ icon: this.weatherIcon.Clouds });
-        break;
-      default:
-        this.setState({ icons: this.weatherIcon.Clouds });
-    }
-  }
-
   getWeather = async e => {
     try {
       e.preventDefault();
@@ -73,42 +37,39 @@ class App extends Component {
       const city = e.target.elements.city.value;
 
       const api_call = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_Key}`
+        `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_Key}`
       );
 
       const response = await api_call.json();
+      const dailyData = response.list.filter(reading =>
+        reading.dt_txt.includes("18:00:00")
+      );
       this.setState({
-        city: response.name,
-        celsius: this.calCelsiuis(response.main.temp),
-        temp_max: this.calCelsiuis(response.main.temp_max),
-        temp_min: this.calCelsiuis(response.main.temp_min),
-        description: response.weather[0].description,
-        error: false
+        city: response.city.name,
+        error: false,
+        fullData: response.list,
+        dailyData: dailyData
       });
-      this.getWeatherIcon(this.weatherIcon, response.weather[0].id);
     } catch {
-      this.setState({ error: true,
-      city:undefined 
-    });
+      this.setState({ error: true, city: undefined });
     }
   };
 
   getWeatherWithLatLng = async pos => {
     const api_call = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${pos.coords.latitude}&lon=${pos.coords.longitude}&appid=${API_Key}`
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${pos.coords.latitude}&lon=${pos.coords.longitude}&appid=${API_Key}`
     );
 
     const response = await api_call.json();
-
+    const dailyData = response.list.filter(reading =>
+      reading.dt_txt.includes("18:00:00")
+    );
     this.setState({
-      city: response.name,
-      celsius: this.calCelsiuis(response.main.temp),
-      temp_max: this.calCelsiuis(response.main.temp_max),
-      temp_min: this.calCelsiuis(response.main.temp_min),
-      description: response.weather[0].description,
-      error: false
+      city: response.city.name,
+      error: false,
+      fullData: response.list,
+      dailyData: dailyData
     });
-    this.getWeatherIcon(this.weatherIcon, response.weather[0].id);
   };
 
   getLocation() {
@@ -118,25 +79,26 @@ class App extends Component {
   }
 
   handleCheckbox = e => {
-  
     if (e.target.checked === true) {
       this.setState({
-        check:e.target.checked
-      })
+        check: e.target.checked
+      });
       this.getLocation();
-    }else{
+    } else {
       this.setState({
         city: undefined,
-        icon: undefined,
-        main: undefined,
-        celsius: undefined,
-        temp_max: undefined,
-        temp_min: undefined,
-        description: "",
         error: false,
-        check:e.target.checked
+        fullData: [],
+        dailyData: [],
+        check: e.target.checked
       });
     }
+  };
+
+  formatDayCards = () => {
+    return this.state.dailyData.map((reading, index) => (
+      <Weather reading={reading} key={index} />
+    ));
   };
 
   render() {
@@ -164,14 +126,12 @@ class App extends Component {
             <Form loadWeather={this.getWeather} error={this.state.error} />
           </div>
           {this.state.city ? (
-            <Weather
-              city={this.state.city}
-              temp_celsius={this.state.celsius}
-              temp_max={this.state.temp_max}
-              temp_min={this.state.temp_min}
-              description={this.state.description}
-              weatherIcon={this.state.icon}
-            />
+            <h2 className="city-header">{this.state.city}</h2>
+          ) : null}
+          {this.state.city ? (
+            <div className="row justify-content-center">
+              {this.formatDayCards()}
+            </div>
           ) : null}
         </div>
       </div>
@@ -180,6 +140,3 @@ class App extends Component {
 }
 
 export default App;
-
-// <input type="checkbox" onChange={e => this.checkItem(e)}></input>
-//           <span class="badge badge-light">Get Location</span>
--- src/weather.js
+++ src/weather.js
@@ -1,34 +1,61 @@
 import React from "react";
 import "./weather.css";
+var moment = require("moment");
 
-const Weather = props => {
+const Weather = ({ reading }) => {
+  let newDate = new Date();
+  const weekday = reading.dt * 1000;
+  newDate.setTime(weekday);
+  const icon = getWeatherIcon(reading.weather[0].id);
+
   return (
-    <div className="card text-center">
-      <div className="card-header">
-        <h4>{props.city}</h4>
-      </div>
-      <div className="card-body">
-        <h5 className="py-2">
-          <i className={`wi ${props.weatherIcon} display-3`}></i>
-        </h5>
-        {props.temp_celsius ? (
-          <h1 className="py-2">{props.temp_celsius}&deg;</h1>
-        ) : null}
-        {minMax(props.temp_min, props.temp_max)}
-        <h5 className="py-3">{props.description}</h5>
+    <div className="col-2 mr-4">
+      <div className="card">
+        <h3 className="card-header">{moment(newDate).format("dddd")}</h3>
+        <p className="text-muted pb-2 pt-2">
+          {moment(newDate).format("MMMM Do")}
+        </p>
+        <i className={`wi ${icon} display-4`}></i>
+        <h3 className="pt-4">{calCelsiuis(reading.main.temp)}&deg;</h3>
+        <p className="card-text pb-10">{reading.weather[0].description}</p>
       </div>
     </div>
   );
 };
 
-function minMax(min, max) {
-  if (max && min) {
-    return (
-      <h3>
-        <span className="px-4">{min}&deg;</span>
-        <span className="px-4">{max}&deg;</span>
-      </h3>
-    );
-  }
+function calCelsiuis(temp) {
+  let cell = Math.floor(temp - 275.15);
+  return cell;
 }
+
+function getWeatherIcon(rangeID) {
+  let icon;
+  switch (true) {
+    case rangeID >= 200 && rangeID <= 232:
+      icon = "wi-thunderstorm";
+      break;
+    case rangeID >= 300 && rangeID <= 321:
+      icon = "wi-sleet";
+      break;
+    case rangeID >= 500 && rangeID <= 531:
+      icon = "wi-storm-showers";
+      break;
+    case rangeID >= 600 && rangeID <= 622:
+      icon = "wi-snow";
+      break;
+    case rangeID >= 701 && rangeID <= 781:
+      icon = "wi-fog";
+      break;
+    case rangeID === 800:
+      icon = "wi-day-sunny";
+      break;
+    case rangeID >= 801 && rangeID <= 804:
+      icon = "wi-day-fog";
+      break;
+    default:
+      icon = "wi-day-fog";
+  }
+  return icon;
+}
+
 export default Weather;
